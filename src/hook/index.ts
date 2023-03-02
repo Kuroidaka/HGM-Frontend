4d5edab850f42e2584e1f96fa69e8f2742d07473
Canh <pcanhgm@gmail.com>
2023-03-03 02:23:36 +0700

    fix import useMedia

--- src/hook/index.ts
+++ src/hook/index.ts
@@ -5,4 +5,4 @@ import UseMedia from '~/hook/useMedia';
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
-export {UseMedia}
+export { UseMedia }
