76bfc59ee3f964e65d0b124bc91b6081a5b1992c
Canh <pcanhgm@gmail.com>
2023-03-25 08:24:00 +0700

    init about us

---
+++ src/feature/user/page/AboutUs/AboutUs.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const AboutUs = () => {
+    
+    return ( 
+        <Container className="about-us-container">
+                
+        </Container>
+     );
+}
+ 
+export default AboutUs;
+
+const Container = styled.div `
+    width: 100vw;
+`
